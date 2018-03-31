130fbf2a4755c7d350a079258526da3ee2ef1370
maxlefebvre <maxlefebvre@cmail.carleton.ca>
2018-03-31 16:05:49 -0400

    alexa hook complete

---
+++ src/index.js
@@ -0,0 +1,72 @@
+/* eslint-disable  func-names */
+/* eslint quote-props: ["error", "consistent"]*/
+/**
+ * This sample demonstrates a simple skill built with the Amazon Alexa Skills
+ * nodejs skill development kit.
+ * This sample supports multiple languages. (en-US, en-GB, de-DE).
+ * The Intent Schema, Custom Slots and Sample Utterances for this skill, as well
+ * as testing instructions are located at https://github.com/alexa/skill-sample-nodejs-fact
+ **/
+
+'use strict';
+const Alexa = require('alexa-sdk');
+const request = require('request');
+
+//=========================================================================================================================================
+//TODO: The items below this comment need your attention.
+//=========================================================================================================================================
+
+//Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.
+//Make sure to enclose your value in quotes, like this: const APP_ID = 'amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1';
+const APP_ID ="amzn1.ask.skill.fdfe99dd-017f-4aee-a287-22f7e731895a";
+
+
+const GET_QUIZ = "Here's your fact quiz question: ";
+const HELP_MESSAGE = 'You can say tell me a space fact, or, you can say exit... What can I help you with?';
+const HELP_REPROMPT = 'What can I help you with?';
+const STOP_MESSAGE = 'Goodbye!';
+
+
+
+//=========================================================================================================================================
+//Editing anything below this line might break your skill.
+//=========================================================================================================================================
+
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.appId = APP_ID;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+const handlers = {
+    'LaunchRequest': function () {
+        this.emit('TellQuizIntent');
+    },
+    'TellQuizIntent': function () {
+        var scope = this;
+        request('https://www.reddit.com/r/dadjokes/top.json?limit=1', function(err, response, body){
+        var resp = JSON.parse(body);
+        var quizTitle = resp.data.children[0].data.title;
+        var quizText = resp.data.children[0].data.selftext;
+
+        var speechOutput = GET_QUIZ + " <break time='1s'/> " + quizTitle + quizText;
+        scope.emit(':tell', speechOutput);
+      })
+    },
+    'AMAZON.HelpIntent': function () {
+        var speechOutput = HELP_MESSAGE;
+        var reprompt = HELP_REPROMPT;
+
+        this.response.speak(speechOutput).listen(reprompt);
+        this.emit(':responseReady');
+    },
+    'AMAZON.CancelIntent': function () {
+        this.response.speak(STOP_MESSAGE);
+        this.emit(':responseReady');
+    },
+    'AMAZON.StopIntent': function () {
+        this.response.speak(STOP_MESSAGE);
+        this.emit(':responseReady');
+    },
+};
