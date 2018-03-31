b39d08e9a93e7fe455bf944a3f7c45f67b6467f2
maxlefebvre <maxlefebvre@cmail.carleton.ca>
2018-03-31 15:03:27 -0400

    for mo

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var http = require('http');
 var router = express.Router();
 
-const API_KEY = 'GPmFgGW4Z6GuhtHtkUuF5F'
+const API_KEY = 'GPmFgGW4Z6GuhtHtkUuF5F';
+const CLIENT_ID = '5up7VNdy4a';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -27,9 +28,9 @@ router.post('/', function (req, res) {
 })
 
 function sendResponse(responseData, res) {
-  responseDataObj = JSON.parse(responseData)
-   if (responseDataObj) {
-    res.render('results', {});
+  // responseDataObj = JSON.parse(responseData)
+   if (responseData) {
+    res.render('results', {title: responseData});
   } else {
       res.render('index');
   }
@@ -37,26 +38,33 @@ function sendResponse(responseData, res) {
 
 function parseData(serchResponse, res) {
   console.log('Parsing...')
+  console.log(serchResponse)
   let responseData = ''
   serchResponse.on('data', function (chunk) {
       responseData += chunk
   })
-  console.log(responseData)
   serchResponse.on('end', function () {
+      console.log(responseData)
       console.log('Sending response...')
       sendResponse(responseData, res)
   })
 }
 
 function searchSets(title, res) {
+  res.redirect(302, `https://quizlet.com/authorize?scope=read&client_id=${CLIENT_ID}&response_type=code&state=cuhacks`)
   console.log('Searching..')
   const options = {
       host: 'api.quizlet.com',
-      path: `/2.0/search/sets?q=${title}&access_token=${API_KEY}`
+      path: `/2.0/search/sets?q=${title}&client_id=${CLIENT_ID}`,
+      authorization: `qizKlXsl4pa4P3WcsDbKRrk0Mks7MOeug1y61lyf`
   }
   http.request(options, function (apiResponse) {
       parseData(apiResponse, res)
   }).end()
 }
+
+// funtion authorizeQuizlet(res){
+//   http.request()
+// }
 
 module.exports = router;
