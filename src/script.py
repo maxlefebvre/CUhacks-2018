8d74dcfb4484ce8580a4f0caea4a25b38c1382f5
maxlefebvre <maxlefebvre@cmail.carleton.ca>
2018-03-31 23:57:20 -0400

    writes bus numbers to file

--- src/script.py
+++ src/script.py
@@ -1,6 +1,12 @@
 from bs4 import BeautifulSoup as BS
 import urllib
 
+thefile = ''
+
 soup = BS(urllib.request.urlopen("http://www.octranspo.com/routes").read())
 contents = [x['value'] for x in soup.find(id="selectRoute").find_all('option')]
 print(contents)
+
+file = open("bus_numbers.txt","w") 
+file.write(str(contents))
+file.close() 
