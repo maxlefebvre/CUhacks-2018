6a382c58833de41387718629da38fde57572c306
maxlefebvre <maxlefebvre@cmail.carleton.ca>
2018-04-01 02:32:39 -0400

    reverted changes

--- src/request.js
+++ src/request.js
@@ -1,50 +1,63 @@
-const oc = require('oc-transpo')
+const oc = require('oc-transpo');
 
-module.exports.setup = oc.setup({
+oc.setup({
     key: "36422cd052fedbe8266b888b26365a19",
     appID: "560500cd"
 });
+setTimeout(doStuff, 4000)
 
-module.exports.getStopSummary = oc.getStopSummary(stopid, function (error, data) {
-    console.log('Get stop summary');
-    if (error) {
-        return console.error(error);
-    }
 
-    return data;
-});
+function getStopSummary(stopid) {
+    oc.getStopSummary(stopid, function (error, data) {
+        console.log('Get stop summary');
+        if (error) {
+            console.error(error);
+            return null;
+        }
 
-module.exports.getRouteInfo = oc.getRouteInformation(stopid, busid, function (error, data) {
-    console.log('Get route info');
-    if (error) {
-        return console.error(error);
-    }
-    console.log('Next bus is in: ' + data.routes[0].trips[0].arrivalTime + ' mins');
-    return data;
-});
+        return console.log(data);
+    });
+}
 
-module.exports.getStopInfo = oc.getStopInformation(stopid, function (error, data) {
-    console.log('Get stop info');
-    if (error) {
-        return console.error(error);
-    }
+function getRouteInfo(stopid, busid) {
+    oc.getRouteInformation(3000, 44, function (error, data) {
+        console.log('Get route info');
+        if (error) {
+            console.error(error);
+            return null;
+        }
+        //console.log('Next bus is in: ' + data.routes[0].trips[0].arrivalTime + ' mins');
+        return console.log(data);
+    });
+}
 
-    return data;
-});
+// function getRemainingTime(stopid, busid) {
+//     data = getRouteInfo(stopid, busid);
+//     if (data) routes = data.routes[0];
+//     else return null;
 
-module.exports.getRemainingTime = oc.getRouteInformation(stopid, busid, function (error, data) {
-    console.log('Get remaining time');
-    if (error) {
-        return console.error(error);
-    }
-    return data.routes[0].trips[0].arrivalTime
-});
-
-// oc.getRouteDirectionIdentifiers(3000, 44, function (error, data) {
-//     console.log('Get route direction');
-//     if (error) {
-//         return console.error(error);
+//     // Check if trips available
+//     if (routes.trips[0]) {
+//         return routes.trips[0].arrivalTime;
+//     } else {
+//         return null;
 //     }
+// }
 
-//     return console.log(data);
-// });
+function getStopInfo(stopid) {
+    oc.getStopInformation(stopid, function (error, data) {
+        console.log('Get stop info');
+        if (error) {
+            console.error(error);
+            return null;
+        }
+
+        return console.log(data);
+    });
+}
+
+function doStuff() {
+    getStopSummary(3000);
+    getRouteInfo(3000, 44);
+    getStopInfo(3000);
+}
