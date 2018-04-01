83a83a41c3d395482e4aa06eb50d58d50d275ad2
maxlefebvre <maxlefebvre@cmail.carleton.ca>
2018-04-01 00:30:17 -0400

    fixed script output to ints on new lines

--- src/script.py
+++ src/script.py
@@ -4,9 +4,10 @@ import urllib
 thefile = ''
 
 soup = BS(urllib.request.urlopen("http://www.octranspo.com/routes").read())
-contents = [x['value'] for x in soup.find(id="selectRoute").find_all('option')]
+contents = [int(x['value']) for x in soup.find(id="selectRoute").find_all('option')]
 print(contents)
 
-file = open("bus_numbers.txt","w") 
-file.write(str(contents))
+file = open("bus_numbers.txt","w")
+for x in contents: 
+    file.write(str(x)+'\n')
 file.close() 
