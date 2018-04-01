b9ff9fcaa6895c7b35a73f226b48fd5cee1055a9
maxlefebvre <maxlefebvre@cmail.carleton.ca>
2018-04-01 01:06:54 -0400

    script takes some stations and outputs to file

--- src/script.py
+++ src/script.py
@@ -1,8 +1,7 @@
 from bs4 import BeautifulSoup as BS
 import urllib
 
-thefile = ''
-
+# Route numbers
 soup = BS(urllib.request.urlopen("http://www.octranspo.com/routes").read())
 contents = [int(x['value']) for x in soup.find(id="selectRoute").find_all('option')]
 print(contents)
@@ -11,3 +10,15 @@ file = open("bus_numbers.txt","w")
 for x in contents: 
     file.write(str(x)+',\n')
 file.close() 
+
+# Station numbers
+soup = BS(urllib.request.urlopen("http://www.octranspo.com/go_mobile/transitway_station_numbers").read())
+table = soup.find("table", {"class","alternating sortable"})
+
+file = open("stop_numbers.txt", "w")
+second = False
+for item in table.findAll('td'):
+    if second:
+        file.write(item.string.strip()+',\n')
+    second = not second
+file.close()
